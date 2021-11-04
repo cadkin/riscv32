e7041ea3fb163591f98f3cd211d6df5a245bed1a
Jianjun99 <55260782+Jianjun99@users.noreply.github.com>
2021-11-04 08:52:15 -0400

    Adding some of the modified fpu riscv logic
    The three file logic added,
    float to int
    int to float
    div

---
+++ src/fpu_src/float_to_int.sv
@@ -0,0 +1,103 @@
+//IEEE Floating Point to Integer Converter (Single Precision)
+//Copyright (C) Jonathan P Dawson 2013
+//2013-12-12
+
+module float_to_int(
+        input logic clk,
+        input logic rst,
+		input logic input_a_stb,output_z_ack,
+		input logic [31:0] input_a,
+		output logic input_a_ack,input_a_ack,
+        output logic [31:0] output_z );
+
+  logic   s_output_z_stb;
+  logic   [31:0] s_output_z;
+  logic   s_input_a_ack;
+
+  logic       [2:0] state;
+  parameter get_a         = 3'd0,
+            special_cases = 3'd1,
+            unpack        = 3'd2,
+            convert       = 3'd3,
+            put_z         = 3'd4;
+
+  logic [31:0] a_m, a, z;
+  logic [8:0] a_e;
+  logic a_s;
+
+  always @(posedge clk)
+  begin
+
+    case(state)
+
+      get_a:
+      begin
+        s_input_a_ack <= 1;
+        if (s_input_a_ack && input_a_stb) begin
+          a <= input_a;
+          s_input_a_ack <= 0;
+          state <= unpack;
+        end
+      end
+
+      unpack:
+      begin
+        a_m[31:8] <= {1'b1, a[22 : 0]};
+        a_m[7:0] <= 0;
+        a_e <= a[30 : 23] - 127;
+        a_s <= a[31];
+        state <= special_cases;
+      end
+
+      special_cases:
+      begin
+        if ($signed(a_e) == -127) begin
+          z <= 0;
+          state <= put_z;
+        end else if ($signed(a_e) > 31) begin
+          z <= 32'h80000000;
+          state <= put_z;
+        end else begin
+          state <= convert;
+        end
+      end
+
+      convert:
+      begin
+        if ($signed(a_e) < 31 && a_m) begin
+          a_e <= a_e + 1;
+          a_m <= a_m >> 1;
+        end else begin
+          if (a_m[31]) begin
+            z <= 32'h80000000;
+          end else begin
+            z <= a_s ? -a_m : a_m;
+          end
+          state <= put_z;
+        end
+      end
+
+      put_z:
+      begin
+        s_output_z_stb <= 1;
+        s_output_z <= z;
+        if (s_output_z_stb && output_z_ack) begin
+          s_output_z_stb <= 0;
+          state <= get_a;
+        end
+      end
+
+    endcase
+
+    if (rst == 1) begin
+      state <= get_a;
+      s_input_a_ack <= 0;
+      s_output_z_stb <= 0;
+    end
+
+  end
+  assign input_a_ack = s_input_a_ack;
+  assign output_z_stb = s_output_z_stb;
+  assign output_z = s_output_z;
+
+endmodule
---
+++ src/fpu_src/int_to_float.sv
@@ -0,0 +1,111 @@
+//IEEE Floating Point Divider (Double Precision)
+//Copyright (C) Jonathan P Dawson 2014
+//all combinational logic int to float
+
+module int_to_float(
+        input logic clk,
+        input logic rst,
+		input logic input_a_stb,output_z_ack,
+		input logic [31:0] input_a,
+		output logic input_a_ack,input_a_ack,
+        output logic [31:0] output_z );
+		
+		logic [31:0] a, z, value,;
+		logic [7:0]  z_r,z_e;
+		logic [23:0] z_m;
+		logic sign,guard, round_bit, sticky;;
+		
+	//assign sign = input_a[31];
+	always @(posedge clk)
+	begin
+	case(state)
+      get_a:
+      begin
+        s_input_a_ack <= 1;
+        if (s_input_a_ack && input_a_stb) begin
+          a <= input_a;
+          s_input_a_ack <= 0;
+          state <= convert_0;
+        end
+      end
+
+      convert_0:
+      begin
+        if ( a == 0 ) begin
+          z_s <= 0;
+          z_m <= 0;
+          z_e <= -127;
+          state <= pack;
+        end else begin
+          value <= a[31] ? -a : a;
+          z_s <= a[31];
+          state <= convert_1;
+        end
+      end
+
+      convert_1:
+      begin
+        z_e <= 31;
+        z_m <= value[31:8];
+        z_r <= value[7:0];
+        state <= convert_2;
+      end
+
+      convert_2:
+      begin
+        if (!z_m[23]) begin
+          z_e <= z_e - 1;
+          z_m <= z_m << 1;
+          z_m[0] <= z_r[7];
+          z_r <= z_r << 1;
+        end else begin
+          guard <= z_r[7];
+          round_bit <= z_r[6];
+          sticky <= z_r[5:0] != 0;
+          state <= round;
+        end
+      end
+
+      round:
+      begin
+        if (guard && (round_bit || sticky || z_m[0])) begin
+          z_m <= z_m + 1;
+          if (z_m == 24'hffffff) begin
+            z_e <=z_e + 1;
+          end
+        end
+        state <= pack;
+      end
+
+      pack:
+      begin
+        z[22 : 0] <= z_m[22:0];
+        z[30 : 23] <= z_e + 127;
+        z[31] <= z_s;
+        state <= put_z;
+      end
+
+      put_z:
+      begin
+        s_output_z_stb <= 1;
+        s_output_z <= z;
+        if (s_output_z_stb && output_z_ack) begin
+          s_output_z_stb <= 0;
+          state <= get_a;
+        end
+      end
+
+    endcase
+
+    if (rst == 1) begin
+      state <= get_a;
+      s_input_a_ack <= 0;
+      s_output_z_stb <= 0;
+    end
+
+  end
+  assign input_a_ack = s_input_a_ack;
+  assign output_z_stb = s_output_z_stb;
+  assign output_z = s_output_z;
+
+endmodule
