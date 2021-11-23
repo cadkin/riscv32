c954b7f3c73e8f76418cda8244665cd6c391e964
Tanner Joseph Fowler <tfowler6@vlsi9.eecs.utk.edu>
2021-11-22 22:16:41 -0500

    ther

--- src/fpu_src/FPU/FPU.sim/sim_1/behav/xsim/xsim.dir/fp_top_sim_behav/webtalk/xsim_webtalk.tcl
+++ src/fpu_src/FPU/FPU.sim/sim_1/behav/xsim/xsim.dir/fp_top_sim_behav/webtalk/xsim_webtalk.tcl
@@ -1,6 +1,6 @@
 webtalk_init -webtalk_dir /home/tfowler6/riscv32/src/fpu_src/FPU/FPU.sim/sim_1/behav/xsim/xsim.dir/fp_top_sim_behav/webtalk/
 webtalk_register_client -client project
-webtalk_add_data -client project -key date_generated -value "Mon Nov 22 13:11:11 2021" -context "software_version_and_target_device"
+webtalk_add_data -client project -key date_generated -value "Mon Nov 22 15:49:45 2021" -context "software_version_and_target_device"
 webtalk_add_data -client project -key product_version -value "XSIM v2020.2 (64-bit)" -context "software_version_and_target_device"
 webtalk_add_data -client project -key build_version -value "3064766" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_platform -value "LIN64" -context "software_version_and_target_device"
@@ -14,7 +14,7 @@ webtalk_add_data -client project -key target_package -value "not_applicable" -co
 webtalk_add_data -client project -key target_speed -value "not_applicable" -context "software_version_and_target_device"
 webtalk_add_data -client project -key random_id -value "43c60cc1-7122-4f94-8d78-1ffbc881a8ba" -context "software_version_and_target_device"
 webtalk_add_data -client project -key project_id -value "8b276f56865043e2b299f678c406824b" -context "software_version_and_target_device"
-webtalk_add_data -client project -key project_iteration -value "37" -context "software_version_and_target_device"
+webtalk_add_data -client project -key project_iteration -value "38" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_name -value "RedHatEnterpriseServer" -context "user_environment"
 webtalk_add_data -client project -key os_release -value "Red Hat Enterprise Linux Server release 7.9 (Maipo)" -context "user_environment"
 webtalk_add_data -client project -key cpu_name -value "Intel Xeon Processor (Skylake, IBRS)" -context "user_environment"
@@ -37,7 +37,7 @@ webtalk_add_data -client xsim -key Total_Nets -value "0" -context "xsim\\usage"
 webtalk_add_data -client xsim -key Total_Processes -value "155" -context "xsim\\usage"
 webtalk_add_data -client xsim -key Total_Instances -value "19" -context "xsim\\usage"
 webtalk_add_data -client xsim -key Xilinx_HDL_Libraries_Used -value "secureip unimacro_ver unisims_ver " -context "xsim\\usage"
-webtalk_add_data -client xsim -key Compiler_Time -value "3.60_sec" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Compiler_Memory -value "413292_KB" -context "xsim\\usage"
-webtalk_transmit -clientid 671355784 -regid "210769129_1777527718_210710220_004" -xml /home/tfowler6/riscv32/src/fpu_src/FPU/FPU.sim/sim_1/behav/xsim/xsim.dir/fp_top_sim_behav/webtalk/usage_statistics_ext_xsim.xml -html /home/tfowler6/riscv32/src/fpu_src/FPU/FPU.sim/sim_1/behav/xsim/xsim.dir/fp_top_sim_behav/webtalk/usage_statistics_ext_xsim.html -wdm /home/tfowler6/riscv32/src/fpu_src/FPU/FPU.sim/sim_1/behav/xsim/xsim.dir/fp_top_sim_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
+webtalk_add_data -client xsim -key Compiler_Time -value "3.58_sec" -context "xsim\\usage"
+webtalk_add_data -client xsim -key Compiler_Memory -value "413316_KB" -context "xsim\\usage"
+webtalk_transmit -clientid 3717081730 -regid "210769129_1777527718_210710220_004" -xml /home/tfowler6/riscv32/src/fpu_src/FPU/FPU.sim/sim_1/behav/xsim/xsim.dir/fp_top_sim_behav/webtalk/usage_statistics_ext_xsim.xml -html /home/tfowler6/riscv32/src/fpu_src/FPU/FPU.sim/sim_1/behav/xsim/xsim.dir/fp_top_sim_behav/webtalk/usage_statistics_ext_xsim.html -wdm /home/tfowler6/riscv32/src/fpu_src/FPU/FPU.sim/sim_1/behav/xsim/xsim.dir/fp_top_sim_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
 webtalk_terminate
