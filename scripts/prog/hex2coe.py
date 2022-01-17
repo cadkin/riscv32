27b61799fe28debf43f9e8b74422a23dfe9647b5
jjting <jting@vols.utk.edu>
2022-01-17 15:30:50 -0600

    Add comments to hex2coe.py

--- scripts/prog/hex2coe.py
+++ scripts/prog/hex2coe.py
@@ -3,17 +3,21 @@ import os
 import json
 import pathlib
 
+# Header lines for .coe files.
 base_str = 'memory_initialization_radix=16;\nmemory_initialization_vector=\n'
+
+# Get path for C code directory.
 base_dir = str(pathlib.Path(os.path.join(sys.path[0], '../../c/')).resolve())
 
+# Get path for Vivado project from config.
 path_cfg = json.load(open(os.path.join(sys.path[0], 'loadcoe.json')))
 proj_path = pathlib.Path(path_cfg['path'])
 absolute_path = proj_path.expanduser().resolve()
 folder_name = proj_path.name
 proj_str = str(absolute_path) + '/' + folder_name
 
+# Read in and split .hex file in four.
 mems = [list(), list(), list(), list()] 
-
 with open(sys.argv[1], 'r') as f:
 	lines = f.readlines() 
 	for l in lines:
@@ -23,6 +27,7 @@ with open(sys.argv[1], 'r') as f:
 		mems[2].append(l[2:4])
 		mems[3].append(l[0:2])
 
+# Create 4 .coe files.
 idx = 0
 out_file_base = os.path.splitext(sys.argv[1])[0]
 for m in mems:
@@ -34,14 +39,16 @@ for m in mems:
 		f.write(';')
 	idx += 1
 
+# Load in template for loadcoe.tcl.
 with open(os.path.join(sys.path[0], 'tcl/loadcoe_base.tcl'), 'r') as f:
 	coe_script = f.read() 
 
+# Insert .coe and Vivado Project paths into template.
 for i in range(4):
 	out_file_str = base_dir + '/' + out_file_base + str(i) + '.coe'
 	coe_script = coe_script.replace('%' + str(i), out_file_str)
-
 coe_script = coe_script.replace('%PATH', proj_str)
 
+# Create loadcoe.tcl
 with open(os.path.join(sys.path[0], 'tcl/loadcoe.tcl'), 'w') as f:
 	f.write(coe_script)
