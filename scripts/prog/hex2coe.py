f91335d0190220531ccef37da4eedd0a7cb42975
jjting <jting@vols.utk.edu>
2022-01-17 14:47:19 -0600

    Refactored hex2coe.py to remove hardcoded paths.

--- scripts/prog/hex2coe.py
+++ scripts/prog/hex2coe.py
@@ -1,9 +1,16 @@
 import sys
 import os
+import json
+import pathlib
 
-basestr = 'memory_initialization_radix=16;\nmemory_initialization_vector=\n'
-#basedir = 'C:/Users/grayb/Projects/Mini-Risc-V-Uart-Srcs/gcc/'
-basedir = '/home/jting/Projects/senecautk/riscv32/c/'
+base_str = 'memory_initialization_radix=16;\nmemory_initialization_vector=\n'
+base_dir = str(pathlib.Path(os.path.join(sys.path[0], '../../c/')).resolve())
+
+path_cfg = json.load(open(os.path.join(sys.path[0], 'loadcoe.json')))
+proj_path = pathlib.Path(path_cfg['path'])
+absolute_path = proj_path.expanduser().resolve()
+folder_name = proj_path.name
+proj_str = str(absolute_path) + '/' + folder_name
 
 mems = [list(), list(), list(), list()] 
 
@@ -17,23 +24,24 @@ with open(sys.argv[1], 'r') as f:
 		mems[3].append(l[0:2])
 
 idx = 0
-outfile_base = os.path.splitext(sys.argv[1])[0]
+out_file_base = os.path.splitext(sys.argv[1])[0]
 for m in mems:
-	outfilestr = outfile_base + str(idx) + '.coe'
-	with open(outfilestr, 'w') as f:
-		f.write(basestr)
+	out_file_str = out_file_base + str(idx) + '.coe'
+	with open(out_file_str, 'w') as f:
+		f.write(base_str)
 		for b in m:
 			f.write(b + '\n')
 		f.write(';')
 	idx += 1
 
 with open(os.path.join(sys.path[0], 'tcl/loadcoe_base.tcl'), 'r') as f:
-	coescript = f.read() 
+	coe_script = f.read() 
 
 for i in range(4):
-	outfilestr = basedir + outfile_base + str(i) + '.coe'
-	coescript = coescript.replace('%' + str(i), outfilestr)
+	out_file_str = base_dir + '/' + out_file_base + str(i) + '.coe'
+	coe_script = coe_script.replace('%' + str(i), out_file_str)
+
+coe_script = coe_script.replace('%PATH', proj_str)
 
 with open(os.path.join(sys.path[0], 'tcl/loadcoe.tcl'), 'w') as f:
-	f.write(coescript)
-
+	f.write(coe_script)
