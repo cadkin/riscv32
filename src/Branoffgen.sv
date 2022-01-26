c42560376591ed337c46995f3091cd97d6d8350a
jjting <jting@vols.utk.edu>
2022-01-26 11:55:17 -0600

    Remove commented out code from Branoffgen.sv

--- src/Branoffgen.sv
+++ src/Branoffgen.sv
@@ -36,9 +36,7 @@ module Branoffgen
     logic [31:0] imm;
     assign branoff_branch= comp_sig ? comp_imm : {{20{ins[31]}},ins[7],ins[30:25],ins[11:8],1'b0};
     assign branoff_jal= comp_sig ? comp_imm : {{12{ins[31]}},ins[19:12],ins[20],ins[30:21],1'b0};
-//    assign branoff_jalr=rs1_mod+{20'h0,ins[31:20]};
     assign imm = comp_sig ? comp_imm : (ins[31] ? {20'hfffff, ins[31:20]} : {20'h00000, ins[31:20]});
     assign branoff_jalr=rs1_mod+imm;
-//    assign branoff=jal?branoff_jal[11:0]:jalr?branoff_jalr[11:0]:branoff_branch[11:0];
     assign branoff = jal ? branoff_jal : jalr ? branoff_jalr : branoff_branch;                                                             
 endmodule: Branoffgen
