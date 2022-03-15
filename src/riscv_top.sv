8b7a233820260257cfb6dcc45414c9cdd7d6ca31
jjting <jting@vols.utk.edu>
2022-03-15 15:02:51 -0500

    Separated 7 segment logic into its own module

--- src/riscv_top.sv
+++ src/riscv_top.sv
@@ -183,7 +183,6 @@ module riscv_top (
 
   logic prog;
   logic [31:0] debug_output;
-  logic [3:0] seg_cur, seg_nxt;
   logic clk_50M, clk_12M, clk_115k;
   logic clk_7seg;
   logic addr_dn, addr_up;
@@ -197,18 +196,6 @@ module riscv_top (
   logic scan_out;
 
   logic spi_mosi, spi_miso, spi_cs, spi_sck;
-
-  typedef enum logic [7:0] {
-    a0 = 8'b11111110,
-    a1 = 8'b11111101,
-    a2 = 8'b11111011,
-    a3 = 8'b11110111,
-    a4 = 8'b11101111,
-    a5 = 8'b11011111,
-    a6 = 8'b10111111,
-    a7 = 8'b01111111
-  } an_e;
-  an_e an_cur, an_nxt;
 
   riscv_bus_if rbus (
       .clk(clk_rv),
@@ -246,6 +233,14 @@ module riscv_top (
       .clk_out(clk_uart)
   );  // 25 MHz -> 115200 kHz
 
+  sev_seg ss0 (
+      .clk(clk_7seg),
+      .rst(Rst),
+      .din(debug_output),
+      .sev_out(sev_out),
+      .an_out(an)
+  );
+
   riscv_core rv_core (rbus.core);
 
   mem_controller memcon0 (
@@ -282,8 +277,6 @@ module riscv_top (
   assign led = {12'h0, rbus.stack_mismatch, mbus.RAS_ena, rbus.trapping, rbus.uart_IRQ};
   assign debug_output = (prog | debug) ? rbus.debug_output : mbus.disp_out;
 
-  assign an = an_cur;
-
   // SPI
   assign spi_miso = miso;
   assign mosi = spi_mosi;
@@ -293,79 +286,6 @@ module riscv_top (
   assign prog = scan_en;
 
   assign clk_rv = clk;
-
-  always_comb begin
-    case (seg_cur)
-      4'b0000: sev_out = 7'b0000001;
-      4'b0001: sev_out = 7'b1001111;
-      4'b0010: sev_out = 7'b0010010;
-      4'b0011: sev_out = 7'b0000110;
-      4'b0100: sev_out = 7'b1001100;
-      4'b0101: sev_out = 7'b0100100;
-      4'b0110: sev_out = 7'b0100000;
-      4'b0111: sev_out = 7'b0001111;
-      4'b1000: sev_out = 7'b0000000;
-      4'b1001: sev_out = 7'b0000100;
-      4'b1010: sev_out = 7'b0001000;
-      4'b1011: sev_out = 7'b1100000;
-      4'b1100: sev_out = 7'b0110001;
-      4'b1101: sev_out = 7'b1000010;
-      4'b1110: sev_out = 7'b0110000;
-      4'b1111: sev_out = 7'b0111000;
-      default: sev_out = 7'b0000000;
-    endcase
-  end
-
-  always_comb begin
-    case (an_cur)
-      a0: begin
-        an_nxt  = a1;
-        seg_nxt = debug_output[7:4];
-      end
-      a1: begin
-        an_nxt  = a2;
-        seg_nxt = debug_output[11:8];
-      end
-      a2: begin
-        an_nxt  = a3;
-        seg_nxt = debug_output[15:12];
-      end
-      a3: begin
-        an_nxt  = a4;
-        seg_nxt = debug_output[19:16];
-      end
-      a4: begin
-        an_nxt  = a5;
-        seg_nxt = debug_output[23:20];
-      end
-      a5: begin
-        an_nxt  = a6;
-        seg_nxt = debug_output[27:24];
-      end
-      a6: begin
-        an_nxt  = a7;
-        seg_nxt = debug_output[31:28];
-      end
-      a7: begin
-        an_nxt  = a0;
-        seg_nxt = debug_output[3:0];
-      end
-      default: begin
-        an_nxt  = a0;
-        seg_nxt = debug_output[3:0];
-      end
-    endcase
-  end
-
-  always_ff @(posedge clk_7seg) begin
-    if (Rst) begin
-      an_cur  <= a0;
-      seg_cur <= debug_output[3:0];
-    end else begin
-      an_cur  <= an_nxt;
-      seg_cur <= seg_nxt;
-    end
-  end
 
   integer cnt = 0;
   integer maxcnt = 100000000;
