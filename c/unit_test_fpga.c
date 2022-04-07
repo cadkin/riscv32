19d3dfbb89df86fe6ac075d1162b7ddd8c9629a7
jjting <jting@vols.utk.edu>
2022-04-07 12:13:02 -0400

    Updated FPGA unit test

--- c/unit_test_fpga.c
+++ c/unit_test_fpga.c
@@ -2,10 +2,13 @@
 #include "uart.h"
 #include "malloc.h"
 #include "qsort.h"
+#include "utils.h"
 #include "tc_hazard.h"
 
 #define DATA_SIZE 10
 
+int m_ext_test(int a);
+int itoa_atoi_test(int a);
 int qsort_test(int a);
 void uart_test();
 
@@ -54,8 +57,18 @@ int main(void)
     itoa(test, num_char);
     uart_print(num_char);
 
+    // M-EXT Test
+    test = m_ext_test(10);
+    itoa(test, num_char);
+    uart_print(num_char);
+
+    // ITOA/ATOI Test
+    test = itoa_atoi_test(11);
+    itoa(test, num_char);
+    uart_print(num_char);
+
     // Qsort Test
-    test = qsort_test(10);
+    test = qsort_test(12);
     itoa(test, num_char);
     uart_print(num_char);
 }
@@ -104,15 +117,126 @@ int qsort_test(int a)
     return result;
 }
 
-void uart_test()
+int m_ext_test(int a)
 {
-    // Initialize UART
-	uart_init();
+    int result = 0;
+    int i = 0;
+    int num = 0;
 
-    // Send back any characters received in UART
-	char c; 
-	while(1) {
-		c = uart_read_blocking();
-		uart_put_blocking(c);
-	}
+    int mul_check[5] = {
+        277284,
+        4112,
+        2052,
+        64,
+        12
+    };
+
+    int div_check[5] = {
+        17330,
+        257,
+        128,
+        4,
+        0
+    };
+
+    int mul_data[5] = {
+        0,
+        0,
+        0,
+        0,
+        0
+    };
+
+    int div_data[5] = {
+        0,
+        0,
+        0,
+        0,
+        0
+    };
+
+    int *input_data = malloc(5 * sizeof(int));
+    input_data[0] = 69321;
+    input_data[1] = 1028;
+    input_data[2] = 513;
+    input_data[3] = 16;
+    input_data[4] = 3;
+
+    num = 4;
+
+    for (i = 0; i < 5; i++)
+    {
+        __asm__
+        (
+            "mul %[c], %[a], %[b]"
+            : [c] "=r" (mul_data[i])
+            : [a] "r" (input_data[i]), [b] "r" (num)
+        );
+        __asm__
+        (
+            "div %[c], %[a], %[b]"
+            : [c] "=r" (div_data[i])
+            : [a] "r" (input_data[i]), [b] "r" (num)
+        );
+    }
+
+    result = a;
+    for (i = 0; i < 5; i++)
+    {
+        if(mul_data[i] != mul_check[i])
+        {
+            result = 0;
+        }
+    }
+
+    for (i = 0; i < 5; i++)
+    {
+        if(div_data[i] != div_check[i])
+        {
+            result = 0;
+        }
+    }
+
+    return result;
+}
+
+int itoa_atoi_test(int a)
+{
+    int result = 0;
+    int i = 0;
+    int num = 0;
+    char numchar[9];
+
+    char *data_check[5];
+    
+    data_check[0] = "277291";
+    data_check[1] = "4112";
+    data_check[2] = "205";
+    data_check[3] = "16";
+    data_check[4] = "3";
+
+    int *input_data = malloc(5 * sizeof(int));
+    input_data[0] = 277291;
+    input_data[1] = 4112;
+    input_data[2] = 205;
+    input_data[3] = 16;
+    input_data[4] = 3;
+
+    result = a;
+    for (i = 0; i < 5; i++)
+    {
+        itoa(input_data[i], numchar);
+        if(strcmp(numchar, data_check[i]) != 0)
+        {
+            result = 0;
+        }
+
+        num = atoi(numchar);
+        if(num != input_data[i])
+        {
+            result = 0;
+        }
+    }
+
+    return result;
 }
